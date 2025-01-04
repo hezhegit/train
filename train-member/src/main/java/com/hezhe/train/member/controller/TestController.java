bec95fb30c81514136d7d5073e390c2052a4fbac
hezhe <3211384971@qq.com>
2025-01-04 22:34:15 +0800

    新增member会员模块

--- train-member/src/main/java/com/hezhe/train/member/controller/TestController.java
+++ train-member/src/main/java/com/hezhe/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.hezhe.train;
+package com.hezhe.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello 123";
+        return "hello";
     }
 }
