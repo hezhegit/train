9c179e8787bb14ce4e7821cb1ee771cbc077b124
hezhe <3211384971@qq.com>
2025-01-19 21:05:49 +0800

    封装分页请求参数

--- train-member/src/main/java/com/hezhe/train/member/req/PassengerQueryReq.java
+++ train-member/src/main/java/com/hezhe/train/member/req/PassengerQueryReq.java
@@ -1,5 +1,6 @@
 package com.hezhe.train.member.req;
 
+import com.hezhe.train.common.req.PageReq;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PassengerQueryReq {
+public class PassengerQueryReq extends PageReq {
 
     private Long memberId;
 
