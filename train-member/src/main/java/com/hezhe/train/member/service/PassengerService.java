39f06066bcc0043f85097a06873554eb4e948ca8
hezhe <3211384971@qq.com>
2025-02-06 16:21:44 +0800

    乘车人编辑接口开发

--- train-member/src/main/java/com/hezhe/train/member/service/PassengerService.java
+++ train-member/src/main/java/com/hezhe/train/member/service/PassengerService.java
@@ -32,11 +32,24 @@ public class PassengerService {
     public void save(PassengerSaveReq req) {
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
-        passenger.setMemberId(LoginMemberContext.getId());
-        passenger.setId(SnowUtil.getSnowflakeNextId());
-        passenger.setCreateTime(now);
-        passenger.setUpdateTime(now);
-        passengerMapper.insert(passenger);
+
+        if (ObjectUtil.isNull(req.getId())) {
+            // 新增
+            passenger.setMemberId(LoginMemberContext.getId());
+            passenger.setId(SnowUtil.getSnowflakeNextId());
+            passenger.setCreateTime(now);
+            passenger.setUpdateTime(now);
+            passengerMapper.insert(passenger);
+        }else {
+            passenger.setUpdateTime(now);
+            // 空值 也会更新
+            passengerMapper.updateByPrimaryKey(passenger);
+            // 空值 保留 原纪录值
+//            passengerMapper.updateByPrimaryKeySelective(passenger);
+        }
+
+
+
     }
 
     public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req) {
