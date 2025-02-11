a2b06380cc69ac51b1003106b13265b7eb2e519c
hezhe <3211384971@qq.com>
2025-02-11 14:34:25 +0800

    增加DbUtil，获取表字段信息

--- generator/src/main/java/com/hezhe/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/hezhe/train/generator/server/ServerGenerator.java
@@ -1,5 +1,7 @@
 package com.hezhe.train.generator.server;
 
+import com.hezhe.train.generator.util.DbUtil;
+import com.hezhe.train.generator.util.Field;
 import com.hezhe.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
@@ -10,6 +12,7 @@ import org.dom4j.io.SAXReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ServerGenerator {
@@ -36,6 +39,17 @@ public class ServerGenerator {
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
 
+        // 为DbUtil设置数据源
+        Node connectionURL = document.selectSingleNode("//@connectionURL");
+        Node userId = document.selectSingleNode("//@userId");
+        Node password = document.selectSingleNode("//@password");
+        System.out.println("url: " + connectionURL.getText());
+        System.out.println("user: " + userId.getText());
+        System.out.println("password: " + password.getText());
+        DbUtil.url = connectionURL.getText();
+        DbUtil.user = userId.getText();
+        DbUtil.password = password.getText();
+
 
         // 示例：表名 jiawa_test
         // Domain = JiawaTest
@@ -44,6 +58,11 @@ public class ServerGenerator {
         String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
         // do_main = jiawa-test
         String do_main = tableName.getText().replaceAll("_", "-");
+
+        // 表中文名
+        String tableNameCn = DbUtil.getTableComment(tableName.getText());
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
+
 
 
         // 组装参数
