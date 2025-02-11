c263f02573487d7b72536fc48ed12cb0c41dfa84
hezhe <3211384971@qq.com>
2025-02-11 10:58:33 +0800

    使用PassengerService制作service模版

--- generator/src/main/java/com/hezhe/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/hezhe/train/generator/server/ServerGenerator.java
@@ -1,5 +1,6 @@
 package com.hezhe.train.generator.server;
 
+import com.hezhe.train.generator.util.FreemarkerUtil;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
@@ -10,30 +11,49 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ServerGenerator {
-    static String toPath = "generator/src/main/java/com/hezhe/train/generator/test/";
+    static String serverPath = "[module]/src/main/java/com/hezhe/train/[module]/service/";
     static String pomPath = "generator/pom.xml";
+    static String module = "";
 
-    static {
-        new File(toPath).mkdirs();
-    }
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws DocumentException {
-
+        // 获取mybatis-generator
         String generatorPath = getGeneratorPath();
+        // 比如generator-config-member.xml，得到module = member
+        module = generatorPath.replace("src/main/resources/generator-config-", "").replace(".xml", "");
+        System.out.println("module: " + module);
+        serverPath = "train-"+ serverPath.replace("[module]", module);
+        new File(serverPath).mkdirs();
+        System.out.println("servicePath: " + serverPath);
 
+        // 读取table节点
         Document document = new SAXReader().read("generator/" + generatorPath);
         Node table = document.selectSingleNode("//table");
         System.out.println(table);
         Node tableName = table.selectSingleNode("@tableName");
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
-        System.out.println(tableName.getText()+"/"+domainObjectName.getText());
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
 
 
-//        FreemarkerUtil.initConfig("test.ftl");
-//        Map<String, Object> param = new HashMap<>();
-////        param.put("basePackage", "com.hezhe.train.generator.server");
-//        param.put("domain", "Test");
-//        FreemarkerUtil.generator(toPath + "Test.java", param);
+        // 示例：表名 jiawa_test
+        // Domain = JiawaTest
+        String Domain = domainObjectName.getText();
+        // domain = jiawaTest
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        // do_main = jiawa-test
+        String do_main = tableName.getText().replaceAll("_", "-");
+
+        // 组装参数
+        Map<String, Object> param = new HashMap<>();
+        param.put("module", module);
+        param.put("Domain", Domain);
+        param.put("domain", domain);
+        param.put("do_main", do_main);
+
+
+
+        FreemarkerUtil.initConfig("service.ftl");
+        FreemarkerUtil.generator(serverPath +Domain+ "Service.java", param);
     }
 
     private static String getGeneratorPath() throws DocumentException {
