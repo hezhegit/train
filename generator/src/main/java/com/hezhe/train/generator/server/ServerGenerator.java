9a10ec6b04962bea36caeef814859190106e52c9
hezhe <3211384971@qq.com>
2025-02-12 14:43:25 +0800

    制作vue.ftl模版，支持只读

--- generator/src/main/java/com/hezhe/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/hezhe/train/generator/server/ServerGenerator.java
@@ -14,6 +14,8 @@ import java.io.IOException;
 import java.util.*;
 
 public class ServerGenerator {
+    static boolean readOnly = false;
+    static String vuePath = "web/src/views/main/";
     static String serverPath = "[module]/src/main/java/com/hezhe/train/[module]/";
     static String pomPath = "generator/pom.xml";
     static String module = "";
@@ -73,14 +75,17 @@ public class ServerGenerator {
         param.put("tableNameCn", tableNameCn);
         param.put("fieldList", fieldList);
         param.put("typeSet", typeSet);
+        param.put("readOnly", readOnly);
         System.out.println("参数: " + param);
 
 
 //        gen(Domain, param, "service", "service");
 //        gen(Domain, param, "controller", "controller");
-        gen(Domain, param, "req", "saveReq");
-        gen(Domain, param, "req", "queryReq");
-        gen(Domain, param, "req", "queryResp");
+//        gen(Domain, param, "req", "saveReq");
+//        gen(Domain, param, "req", "queryReq");
+//        gen(Domain, param, "req", "queryResp");
+
+        genVue(do_main, param);
 
     }
 
@@ -118,4 +123,13 @@ public class ServerGenerator {
         }
         return set;
     }
+
+    private static void genVue(String do_main, Map<String, Object> param) throws IOException, TemplateException {
+        FreemarkerUtil.initConfig("vue.ftl");
+        new File(vuePath).mkdirs();
+        String fileName = vuePath + do_main + ".vue";
+        System.out.println("开始生成：" + fileName);
+        FreemarkerUtil.generator(fileName, param);
+    }
+
 }
