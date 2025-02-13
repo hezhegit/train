bb20a2b10fc4f879f8d2963f4bc03207b98bef7d
hezhe <3211384971@qq.com>
2025-02-13 13:12:41 +0800

    修改生成器，生成admin相关的代码，测试成功

--- train-business/src/main/java/com/hezhe/train/business/controller/admin/StationAdminController.java
+++ train-business/src/main/java/com/hezhe/train/business/controller/admin/StationAdminController.java
@@ -1,4 +1,4 @@
-package com.hezhe.train.business.controller;
+package com.hezhe.train.business.controller.admin;
 
 import com.hezhe.train.business.req.StationQueryReq;
 import com.hezhe.train.business.req.StationSaveReq;
@@ -11,8 +11,8 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/station")
-public class StationController {
+@RequestMapping("/admin/station")
+public class StationAdminController {
 
     @Resource
     private StationService stationService;
