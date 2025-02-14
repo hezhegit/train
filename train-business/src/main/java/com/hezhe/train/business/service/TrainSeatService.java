9897ea39f5ec090e8f6daf3487949cccdc3248c5
hezhe <3211384971@qq.com>
2025-02-14 14:58:07 +0800

    为火车座位页面增加车次查询条件

--- train-business/src/main/java/com/hezhe/train/business/service/TrainSeatService.java
+++ train-business/src/main/java/com/hezhe/train/business/service/TrainSeatService.java
@@ -53,8 +53,14 @@ public class TrainSeatService {
     public PageResp<TrainSeatQueryResp> queryList(TrainSeatQueryReq req) {
         TrainSeatExample example = new TrainSeatExample();
         // id 倒序
-        example.setOrderByClause("id desc");
+        example.setOrderByClause("train_code asc, carriage_index asc, carriage_seat_index asc");
         TrainSeatExample.Criteria criteria = example.createCriteria();
+
+        if (ObjectUtil.isNotEmpty(req.getTrainCode())) {
+            criteria.andTrainCodeEqualTo(req.getTrainCode());
+        }
+
+
         LOG.info("查询页码：{}", req.getPage());
         LOG.info("每页条数：{}", req.getSize());
         PageHelper.startPage(req.getPage(), req.getSize());
