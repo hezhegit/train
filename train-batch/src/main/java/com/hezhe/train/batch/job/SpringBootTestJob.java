fcb15652d99112da9c71762d3a90e0485247aa04
hezhe <3211384971@qq.com>
2025-02-15 21:44:13 +0800

    禁止定时任务并发执行

--- train-batch/src/main/java/com/hezhe/train/batch/job/SpringBootTestJob.java
+++ train-batch/src/main/java/com/hezhe/train/batch/job/SpringBootTestJob.java
@@ -1,23 +1,23 @@
-package com.hezhe.train.batch.job;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-/**
- *  适合单体应用 不适合集群
- *  集群的每个单体都会调用，造成资源浪费
- *  可通过增加分布式锁，确保集群中只有一个在执行 =》解决办法
- *  没法实时更改定时任务状态和策略=》没法解决
- */
-@Component
-@EnableScheduling
-public class SpringBootTestJob {
-
-    // 表示秒数，秒数/5 余数是0，==0， 5， 10...触发
-    @Scheduled(cron = "0/5 * * * * ?")
-    private void test() {
-        System.out.println("SpringBootTestJob test");
-    }
-
-}
+//package com.hezhe.train.batch.job;
+//
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+///**
+// *  适合单体应用 不适合集群
+// *  集群的每个单体都会调用，造成资源浪费
+// *  可通过增加分布式锁，确保集群中只有一个在执行 =》解决办法
+// *  没法实时更改定时任务状态和策略=》没法解决
+// */
+//@Component
+//@EnableScheduling
+//public class SpringBootTestJob {
+//
+//    // 表示秒数，秒数/5 余数是0，==0， 5， 10...触发
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    private void test() {
+//        System.out.println("SpringBootTestJob test");
+//    }
+//
+//}
--- train-batch/src/main/java/com/hezhe/train/batch/job/TestJob.java
+++ train-batch/src/main/java/com/hezhe/train/batch/job/TestJob.java
@@ -1,14 +1,26 @@
 package com.hezhe.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+// DisallowConcurrentExecution 禁止并发执行
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob execute");
+        System.out.println("TestJob execute 开始");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("TestJob execute 结束");
+
     }
 }
