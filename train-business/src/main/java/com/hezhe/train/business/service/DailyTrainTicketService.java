96deab620b788333f88ee703f40fb66086994bbc
hezhe <3211384971@qq.com>
2025-02-28 23:00:38 +0800

    spring缓存使用：@Cacheable保存缓存，@CachePut更新缓存 搭配使用，但是和mybatis缓存一样都存在只有当前节点有效的问题

--- train-business/src/main/java/com/hezhe/train/business/service/DailyTrainTicketService.java
+++ train-business/src/main/java/com/hezhe/train/business/service/DailyTrainTicketService.java
@@ -20,6 +20,7 @@ import com.hezhe.train.common.util.SnowUtil;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -63,6 +64,7 @@ public class DailyTrainTicketService {
 
     }
 
+    @Cacheable(value = "DailyTrainTicketService.queryList")
     public PageResp<DailyTrainTicketQueryResp> queryList(DailyTrainTicketQueryReq req) {
         DailyTrainTicketExample example = new DailyTrainTicketExample();
         // id 倒序
