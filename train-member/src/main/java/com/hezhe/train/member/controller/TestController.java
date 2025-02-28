ad2fd016e4230bf7b208245255b84aef671cfba6
hezhe <3211384971@qq.com>
2025-02-28 11:12:45 +0800

    集成Nacos配置中心，实现动态刷新配置的功能

--- train-member/src/main/java/com/hezhe/train/member/controller/TestController.java
+++ train-member/src/main/java/com/hezhe/train/member/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.hezhe.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
+
+
+    @Value("${test.nacos}")
+    private String nacos;
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello"+nacos;
     }
 }
