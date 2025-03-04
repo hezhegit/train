761533a1f1d4a865544e76b0cca1d9ffd5ca8957
hezhe <3211384971@qq.com>
2025-03-04 19:51:45 +0800

    使用分布式锁 限制一个人不定的下单

--- train-business/src/main/java/com/hezhe/train/business/service/SkTokenService.java
+++ train-business/src/main/java/com/hezhe/train/business/service/SkTokenService.java
@@ -16,12 +16,17 @@ import com.hezhe.train.business.req.SkTokenQueryReq;
 import com.hezhe.train.business.req.SkTokenSaveReq;
 import com.hezhe.train.business.resp.SkTokenQueryResp;
 import jakarta.annotation.Resource;
+import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 
 @Service
 public class SkTokenService {
@@ -38,6 +43,12 @@ public class SkTokenService {
     private DailyTrainStationService dailyTrainStationService;
     @Resource
     private SkTokenMapperCust skTokenMapperCust;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private RedissonClient redissonClient;
 
 
 
@@ -123,7 +134,22 @@ public class SkTokenService {
      * 校验令牌
      */
     public boolean validSkToken(Date date, String trainCode, Long memberId) {
-         // 令牌约等于库存，令牌没有了，就不再卖票，不需要再进入购票主流程去判断库存，判断令牌肯定比判断库存效率高
+        LOG.info("会员【{}】获取日期【{}】车次【{}】的令牌开始", memberId, DateUtil.formatDate(date), trainCode);
+
+
+        // 先获取令牌锁，再校验令牌余量，防止机器人抢票，lockKey就是令牌，用来表示【谁能做什么】的一个凭证
+        String lockKey = DateUtil.formatDate(date) + "-" + trainCode + "-" + memberId;
+        Boolean setIfAbsent = redisTemplate.opsForValue().setIfAbsent(lockKey, lockKey, 5, TimeUnit.SECONDS);
+        if (Boolean.TRUE.equals(setIfAbsent)) {
+            LOG.info("恭喜，抢到令牌锁了！lockKey：{}", lockKey);
+        } else {
+            LOG.info("很遗憾，没抢到令牌锁！lockKey：{}", lockKey);
+            return false;
+        }
+
+
+
+        // 令牌约等于库存，令牌没有了，就不再卖票，不需要再进入购票主流程去判断库存，判断令牌肯定比判断库存效率高
          int updateCount = skTokenMapperCust.decrease(date, trainCode, 1);
          if (updateCount > 0) {
              return true;
